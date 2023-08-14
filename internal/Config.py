d752d6a96f0080c402dda0dcad15bb6a2df8eff1
caoweinan <caoweinan@bailian.ai>
2023-08-14 14:34:04 +0800

    优化

--- internal/Config.py
+++ internal/Config.py
@@ -4,21 +4,22 @@
 class MonkeyConfig(object):
     # 测试的app包名
     # package_name = "com.sdpopen.demo"
-    package_name = "com.zxhd"
+    package_name = "com.mymoney"
 
     # 测试app中模块的关键词
     # module_key = "com.sdpopen.wallet"
-    module_key = "com.zxhd"
+    module_key = "com.mymoney"
 
     # todo 自动找到apk中exported的activity
     # 卡死状态随机跳转的activity,第一个元素为测试初始页
-    activity = ["com.zxhd/.view.main.activity.MainActivity",
-                "com.zxhd/.view.home.activity.CourseListActivity",
-                "com.zxhd/.view.person.activity.UserInfoFixActivity",
-                "com.zxhd/.view.home.activity.CommonWebHActivity",
-                "com.zxhd/.view.person.activity.UserCourseActivity",
-                "com.zxhd/.view.person.activity.UserCandyActivity",
-                "com.zxhd/.view.person.activity.SettingActivity"]
+    activity = ["com.mymoney/.biz.main.MainActivity",
+                "com.mymoney/.biz.navtrans.activity.NavYearTransActivity",
+                "com.mymoney/.biz.basicdatamanagement.biz.account.activity.AccountActivity",
+                "com.mymoney/.loan.biz.activity.MyCashNowMainActivity",
+                "com.mymoney/.biz.finance.FinanceActivity",
+                "com.mymoney/.biz.setting.SettingActivity",
+                "com.mymoney/.biz.addtrans.activity.AddTransActivity",
+                "com.mymoney/.biz.main.bottomboard.SettingBottomBoardActivity"]
 
     monkeyCmd = f"monkey -p {package_name} --throttle 300  " \
                 "--pct-appswitch 5 --pct-touch 30 --pct-motion 60 --pct-anyevent 5  " \
